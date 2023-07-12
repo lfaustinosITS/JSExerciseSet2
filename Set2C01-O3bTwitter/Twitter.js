016cf6b4689b2893adbd32a36fae9fb97d270585
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-12 17:16:20 -0600

    New version of Set2C01-03b
    Now has a web interface

---
+++ Set2C01-O3bTwitter/Twitter.js
@@ -0,0 +1,19 @@
+function replaceWithStringAndLink(text, searchString) {
+  const hashtag = `#${searchString}`;
+  const twitterSearchLink = `https://twitter.com/search?q=${encodeURIComponent(hashtag)}`;
+  const replacedText = text.replace(new RegExp(searchString, 'gi'), `<a href="${twitterSearchLink}" target="_blank">${hashtag}</a>`);
+  return replacedText;
+}
+
+const replaceButton = document.getElementById('replaceButton');
+replaceButton.addEventListener('click', function () {
+  const textInput = document.getElementById('textInput');
+  const searchInput = document.getElementById('searchInput');
+  const resultContainer = document.getElementById('resultContainer');
+
+  const text = textInput.value;
+  const searchString = searchInput.value;
+  const replacedText = replaceWithStringAndLink(text, searchString);
+
+  resultContainer.innerHTML = replacedText;
+});
